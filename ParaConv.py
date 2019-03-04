9c4a62f89ac562ed16fe4989e04e317963ce155e
FUN <fun.public.address@gmail.com>
2019-03-04 15:39:17 +0900

    14個の変換規則をすべて実装。

--- ParaConv.py
+++ ParaConv.py
@@ -48,41 +48,122 @@ def analyze(path, file, out, log, dic):
 			tokens = analyzer.analyze(sentence)
 
 			# Initialize
+			word       = ''
 			preword    = ''
 			prepreword = ''
+			base       = ''
+			prebase    = ''
+			preprebase = ''
+			form       = ''
 			preform    = ''
 			prepreform = ''
+			part       = ['','','','','','','','','']
 			prepart    = ['','','','','','','','','']
 			preprepart = ['','','','','','','','','']
+			convsrc    = ''
+			convdst    = ''
 
 			# Apply Rules
 			for token in tokens:
 
+				# Shift Parameters
+				prepreword = preword
+				preprebase = prebase
+				prepreform = preform
+				preprepart = prepart
+				preword    = word
+				prebase    = base
+				preform    = form
+				prepart    = part
+
+				# Get Parameters
 				word = token.surface
+				base = token.base_form
 				form = token.infl_form
 				part = token.part_of_speech.split(',')[0]
-				convsrc = ''
-				convdst = ''
 
-				print(word, form, part)
-
-				# Rule 1
-				if (part == '動詞' and form == '基本形'):
+				# Rule 12
+				if ((base == 'だ' or base == 'だが') and part == '接続詞'):
 					convsrc = word
-					convdst = dic.get(word + ':' + '連用形') + 'ます'
+					convdst = 'ですが'
 
 				# Replace words
 				if not convsrc == '':
 					logflag = True
 					line = line.replace(convsrc, convdst, 1)
+					convsrc == ''
 
-				# Shift Parameters
-				prepreword = preword
-				prepreform = preform
-				preprepart = prepart
-				preword    = word
-				preform    = form
-				prepart    = part
+			else:
+
+				# Rule 1
+				if (part == '動詞' and form == '基本形'):
+					convsrc = word
+					convdst = dic.get(base + ':' + '連用形') + 'ます'
+
+				# Rule 2
+				elif (prepart == '動詞' and (preform == '連用形' or preform == '連用タ接続') and base == 'た' and part == '助動詞' and form == '基本形'):
+					convsrc = preword + word
+					convdst = dic.get(prebase + ':' + '連用形') + 'ました'
+
+				# Rule 3
+				elif (prepart == '動詞' and preform == '未然形' and base == 'ない' and part == '助動詞' and form == '基本形'):
+					convsrc = preword + word
+					convdst = dic.get(prebase + ':' + '未然形') + 'ません'
+
+				# Rule 4
+				elif (preprepart == '動詞' and prepreform == '未然形' and prebase == 'ない' and prepart == '助動詞' and preform == '連用タ接続' and base == 'た' and part == '助動詞' and form == '基本形'):
+					convsrc = prepreword + preword + word
+					convdst = dic.get(preprebase + ':' + '未然形') + 'ませんでした'
+
+				# Rule 5
+				elif (base == 'だ' and part == '助動詞' and form == '基本形'):
+					convsrc = word
+					convdst = 'です'
+
+				# Rule 6
+				elif (prebase == 'だ' and prepart == '助動詞' and preform == '連用タ接続' and base == 'た' and part == '助動詞' and form == '基本形'):
+					convsrc = preword + word
+					convdst = 'でした'
+
+				# Rule 7
+				elif (base == 'ない' and part == '助動詞' and form == '基本形'):
+					convsrc = word
+					convdst = 'ありません'
+
+				# Rule 8
+				elif (prebase == 'だ' and prepart == '助動詞' and preform == '連用形' and base == 'ある' and part == '助動詞' and form == '基本形'):
+					convsrc = preword + word
+					convdst = 'です'
+
+				# Rule 9
+				elif (preprebase == 'だ' and preprepart == '助動詞' and prepreform == '連用形' and prebase == 'ある' and prepart == '助動詞' and preform == '連用タ接続' and base == 'た' and part == '助動詞' and form == '基本形'):
+					convsrc = prepreword + preword + word
+					convdst = 'でした'
+
+				# Rule 10
+				elif (preprebase == 'だ' and preprepart == '助動詞' and prepreform == '連用形' and prebase == 'ある' and prepart == '助動詞' and preform == '未然ウ接続' and base == 'う' and part == '助動詞' and form == '基本形'):
+					convsrc = prepreword + preword + word
+					convdst = 'でしょう'
+
+				# Rule 11
+				elif (prebase == 'だ' and prepart == '助動詞' and preform == '未然形' and base == 'う' and part == '助動詞' and form == '基本形'):
+					convsrc = preword + word
+					convdst = 'でしょう'
+
+				# Rule 13
+				elif (base == 'ない' and part == '形容詞' and form == '基本形'):
+					convsrc = word
+					convdst = 'ありません'
+
+				# Rule 14
+				elif (prebase == 'ない' and prepart == '形容詞' and preform == '連用タ接続' and base == 'た' and part == '助動詞' and form == '基本形'):
+					convsrc = preword + word
+					convdst = 'ありませんでした'
+
+				# Replace words
+				if not convsrc == '':
+					logflag = True
+					line = line.replace(convsrc, convdst, 1)
 
 		# Output
 		out.write(line + '\n')
@@ -90,7 +171,7 @@ def analyze(path, file, out, log, dic):
 		# Logging
 		if logflag:
 			log.write('- ' + srcline)
-			log.write('+ ' + line + '\n')
+			log.write('+ ' + line + '\n\n')
 
 	text.close()
 
